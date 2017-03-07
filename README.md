837d28044b34d56aa2b99e5b641b866919f503a4
Galen Cook <galenscook@gmail.com>
2017-03-07 10:04:15 -0800

    strpcy

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cpractice
+
+Right now, just working through the exercises [here](https://remind.quip.com/7zoFACUB1a4x).
+
+## Running programs
+
+```
+make <file_name>
+./<file_name>
+```
---
+++ strcpy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void mystrcpy(char *dest, const char *src) {
+    int i;
+    for (i=0; src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("ERROR: You need one argument.\n");
+        return 1;
+    }
+
+    char *src = argv[1];
+    char *dest = malloc(strlen(src) + 1);
+
+    mystrcpy(dest, src);
+
+    printf("This is my copied string : %s\n", dest);
+
+    free(dest);
+
+    return 0;
+}
